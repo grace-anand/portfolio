28c3a89250e9e6d23b4f553b7a023556ceee2c4d
Anand R <graceanand99@gmail.com>
2023-08-18 23:22:29 +0530

    Added Smoothscroll to with Lenis

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="astro/client" />
+// for lenis <script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@1/bundled/lenis.min.js" async></script>
+class Lenis {
+  on(event: 'scroll', callback: (e: ScrollEvent) => void): void;
+  raf (time: number): void;
+}
