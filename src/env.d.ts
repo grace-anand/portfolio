57a00bafdeb7c0f2adaa07736cef6a671567fec0
Anand R <graceanand99@gmail.com>
2023-08-19 21:30:27 +0530

    Revert "Added Smoothscroll to with Lenis"
    This reverts commit 28c3a89250e9e6d23b4f553b7a023556ceee2c4d.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="astro/client" />
-// for lenis <script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@1/bundled/lenis.min.js" async></script>
-class Lenis {
-  on(event: 'scroll', callback: (e: ScrollEvent) => void): void;
-  raf (time: number): void;
-}
